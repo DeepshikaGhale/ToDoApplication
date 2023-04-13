2d2d2598e29b506961237820eb998ba5eb750ce8
Deepshika Ghale <58839663+DeepshikaGhale@users.noreply.github.com>
2023-04-12 22:04:36 -0400

    Add constraints to model

--- ToDoApplication/Data/ToDoContext.cs
+++ ToDoApplication/Data/ToDoContext.cs
@@ -1,11 +1,22 @@
 ﻿using System;
+using System.Reflection.Metadata;
+using Microsoft.EntityFrameworkCore;
+using ToDoApplication.Data;
+using ToDoApplication.Models;
+
 namespace ToDoApplication.Data
 {
-	public class ToDoContext
+	public class ToDoContext : DbContext
 	{
-		public ToDoContext()
-		{
-		}
-	}
+        public ToDoContext(DbContextOptions<ToDoContext> options)
+           : base(options) //sends the options to the db context , this makes the configuration works externally
+        {
+
+        }
+        //creates a table caleed Blogs
+        public DbSet<ToDo> Blogs => Set<ToDo>();
+
+    }
 }
+
 
--- ToDoApplication/Models/ToDo.cs
+++ ToDoApplication/Models/ToDo.cs
@@ -1,19 +1,22 @@
-﻿using System.Xml.Serialization;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ToDoApplication.Models
 {
 	public class ToDo
 	{
+		[Key]
 		public int Id { get; set; }
 
-		public DateTime CompletionDate { get; set; } = DateTime.MinValue;
+        [DataType(DataType.DateTime)]
+        public DateTime CompletionDate { get; set; } = DateTime.MinValue;
 
+		[Required]
 		public string Title { get; set; } = String.Empty;
+
 
 		public string Description { get; set; } = String.Empty;
 
+		[Required]
 		public bool IsComplete { get; set; } = false;
 	}
 }
-
