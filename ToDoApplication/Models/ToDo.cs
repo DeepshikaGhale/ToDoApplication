24ec6f390d0b04fc6562ca5034b73f499070401a
Deepshika Ghale <58839663+DeepshikaGhale@users.noreply.github.com>
2023-04-12 20:23:57 -0400

    Create model class

---
+++ ToDoApplication/Models/ToDo.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.Serialization;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoApplication.Models
+{
+	public class ToDo
+	{
+		public int Id { get; set; }
+
+		public DateTime CompletionDate { get; set; } = DateTime.MinValue;
+
+		public string Title { get; set; } = String.Empty;
+
+		public string Description { get; set; } = String.Empty;
+
+		public bool IsComplete { get; set; } = false;
+	}
+}
+
